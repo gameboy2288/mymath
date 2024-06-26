3ec3ac87787b4a45b8ed7a1a7d5dc3236d00e81d
Lucifer <lucifer@kali.luciferhassai.com>
2024-06-26 15:31:53 -0700

    v1.0.0

---
+++ main.go
@@ -0,0 +1,47 @@
+package mymath
+
+import "math"
+
+func Abs(x float64) float64 {
+	return math.Abs(x)
+}
+
+func Acos(x float64) float64 {
+	return math.Acos(x)
+}
+
+func Acosh(x float64) float64 {
+	return math.Acosh(x)
+}
+
+func Asin(x float64) float64 {
+	return math.Asin(x)
+}
+
+func Sqrt(x float64) float64 {
+	return math.Sqrt(x)
+}
+
+func Max(x, y float64) float64 {
+	return math.Max(x, y)
+}
+
+func Yn(n int, x float64) float64 {
+	return math.Yn(n, x)
+}
+
+func Ceil(x float64) float64 {
+	return math.Ceil(x)
+}
+
+func Floor(x float64) float64 {
+	return math.Floor(x)
+}
+
+func Pow(x, y float64) float64 {
+	return math.Pow(x, y)
+}
+
+func Min(x, y float64) float64 {
+	return math.Min(x, y)
+}
